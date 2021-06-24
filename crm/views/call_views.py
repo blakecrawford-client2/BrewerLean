f7e88447f8d7f147483f9d4c6ed5af1d359cb2f1
blake <blake@alementary.com>
2021-06-24 15:27:13 -0400

    bug fixes from Basecampl

--- crm/views/call_views.py
+++ crm/views/call_views.py
@@ -164,11 +164,12 @@ class CallsListView(LoginRequiredMixin, ListView):
         context = super(CallsListView, self).get_context_data(**kwargs)
         context['page_name'] = 'Calls List'
         #open_calls = Call.objects.filter(account__ale_owner=current_user, type__isnull=False, outcome__isnull=True)
-        overdue_calls = Call.objects.filter(account__ale_owner=current_user, outcome__isnull=True, schedule_week_monday__lt=this_week_monday)
+        overdue_calls = Call.objects.filter(account__ale_owner=current_user, schedule_week_monday__lt=this_week_monday, outcome__isnull=True)
         original_calls = Call.objects.filter(account__ale_owner=current_user, type__isnull=False, outcome__isnull=True)
         follow_up_this_week_calls = Call.objects.filter(account__ale_owner=current_user, schedule_week_monday=this_week_monday, type__isnull=True,  outcome__isnull=True)
         follow_up_next_week_calls = Call.objects.filter(account__ale_owner=current_user, schedule_week_monday=next_week_monday, type__isnull=True, outcome__isnull=True)
         #context['open_calls'] = open_calls
+        context['overdue_calls'] = overdue_calls
         context['original_calls'] = original_calls
         context['fup_this_week'] = follow_up_this_week_calls
         context['fup_next_week'] = follow_up_next_week_calls
